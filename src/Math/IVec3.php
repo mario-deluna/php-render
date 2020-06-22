3c794c62f0c0ca9535cf318ef94b87fc04cc54cb
Mario Döring <deluna.yy@gmail.com>
2020-06-22 21:12:44 +0200

    Vector class builder

--- src/Math/IVec3.php
+++ src/Math/IVec3.php
@@ -2,13 +2,13 @@
 
 namespace PHPR\Math;
 
-class Vec3 
+class IVec3 
 {
-    public float $x;
-    public float $y;
-    public float $z;
+    public int $x;
+    public int $y;
+    public int $z;
 
-    public function __construct(float $x, float $y, float $z) 
+    public function __construct(int $x, int $y, int $z) 
     {
         $this->x = $x;
         $this->y = $y;
@@ -18,9 +18,9 @@ class Vec3
     /**
      * Add two vectors together
      */
-    public static function _add(Vec3 $left, Vec3 $right, ?Vec3 &$result = null)
+    public static function _add(IVec3 $left, IVec3 $right, ?IVec3 &$result = null)
     {
-        if (is_null($result)) $result = new Vec3(0, 0, 0);
+        if (is_null($result)) $result = new IVec3(0, 0, 0);
         
         $result->x = $left->x + $right->x;
         $result->y = $left->y + $right->y;
@@ -32,17 +32,17 @@ class Vec3
     /**
      * Add a vector to the current one
      */
-    public function add(Vec3 $right)
+    public function add(IVec3 $right)
     {
-        Vec3::_add($this, $right, $this); return $this;
+        IVec3::_add($this, $right, $this); return $this;
     }
     
     /**
      * Substract a vector of another one
      */
-    public static function _substract(Vec3 $left, Vec3 $right, ?Vec3 &$result = null)
+    public static function _substract(IVec3 $left, IVec3 $right, ?IVec3 &$result = null)
     {
-        if (is_null($result)) $result = new Vec3(0, 0, 0);
+        if (is_null($result)) $result = new IVec3(0, 0, 0);
         
         $result->x = $left->x - $right->x;
         $result->y = $left->y - $right->y;
@@ -54,17 +54,17 @@ class Vec3
     /**
      * Substract a vector to the current one
      */
-    public function substract(Vec3 $right)
+    public function substract(IVec3 $right)
     {
-        Vec3::_substract($this, $right, $this); return $this;
+        IVec3::_substract($this, $right, $this); return $this;
     }
     
     /**
      * Multiply a vector by a scalar value
      */
-    public static function _multiply(Vec3 $left, float $value, ?Vec3 &$result = null)
+    public static function _multiply(IVec3 $left, int $value, ?IVec3 &$result = null)
     {
-        if (is_null($result)) $result = new Vec3(0, 0, 0);
+        if (is_null($result)) $result = new IVec3(0, 0, 0);
         
         $result->x = $left->x * $value;
         $result->y = $left->y * $value;
@@ -76,19 +76,19 @@ class Vec3
     /**
      * Multiply the current vector by a scalar value
      */
-    public function multiply(float $value)
+    public function multiply(int $value)
     {
-        Vec3::_multiply($this, $value, $this); return $this;
+        IVec3::_multiply($this, $value, $this); return $this;
     }
     
     /**
      * Divide a vector by a scalar value
      */
-    public static function _divide(Vec3 $left, float $value, ?Vec3 &$result = null)
+    public static function _divide(IVec3 $left, int $value, ?IVec3 &$result = null)
     {
         if ($value == 0) throw new \Exception("Division by zero. Please don't...");
 
-        if (is_null($result)) $result = new Vec3(0, 0, 0);
+        if (is_null($result)) $result = new IVec3(0, 0, 0);
         
         $result->x = $left->x / $value;
         $result->y = $left->y / $value;
@@ -100,9 +100,9 @@ class Vec3
     /**
      * Divide the current vector by a scalar value
      */
-    public function divide(float $value)
+    public function divide(int $value)
     {
-        Vec3::_divide($this, $value, $this); return $this;
+        IVec3::_divide($this, $value, $this); return $this;
     }
 
     /**
